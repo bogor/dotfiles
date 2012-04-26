f7151e175ae0a4bcc11a0dc57844ff1994ac9837
bogor <galt32@gmail.com>
2012-04-27 00:44:10 +0300

    ready to roll!

--- kickoff.sh
+++ kickoff.sh
@@ -10,7 +10,8 @@ mkdir -p ~/src
 mkdir -p ~/lib
 
 # get muh dotfilez
-# git clone http://bitbucket.org/galt32/cfg ~/lib/cfg
+git clone https://bogor@github.com/bogor/dotfiles.git ~/lib/cfg
+git clone https://bogor@github.com/bogor/oh-my-zsh.git ~/.oh-my-zsh
 
 # build some vim paths
 mkdir -p $HOME/lib/cfg/vim/tmp/undo
@@ -22,15 +23,15 @@ mkdir -p $HOME/lib/cfg/vim/bundle
 # Link everything to muh cfg dir
 #---------------------------------
 
-ln -s "$HOME/lib/cfg/.zshrc"             "$HOME/.zshrc"
-ln -s "$HOME/lib/cfg/.bashrc"            "$HOME/.bashrc"
+ln -s "$HOME/lib/cfg/zshrc"             "$HOME/.zshrc"
+ln -s "$HOME/lib/cfg/bashrc"            "$HOME/.bashrc"
 ln -s "$HOME/lib/cfg/vim"                "$HOME/.vim"
 ln -s "$HOME/lib/cfg/vim/vimrc"          "$HOME/.vimrc"
 
-ln -s "$HOME/lib/cfg/.gitconfig"         "$HOME/.gitconfig"
-ln -s "$HOME/lib/cfg/.gitignore_global"  "$HOME/.gitignore_global"
-ln -s "$HOME/lib/cfg/.hgrc"              "$HOME/.hgrc"
-ln -s "$HOME/lib/cfg/.hgignore_global"   "$HOME/.hgignore_global"
+ln -s "$HOME/lib/cfg/gitconfig"         "$HOME/.gitconfig"
+ln -s "$HOME/lib/cfg/gitignore_global"  "$HOME/.gitignore_global"
+ln -s "$HOME/lib/cfg/hgrc"              "$HOME/.hgrc"
+ln -s "$HOME/lib/cfg/hgignore_global"   "$HOME/.hgignore_global"
 
 
 #------------------------------
